5605302580ccb2965ce0fad472342eb173f6a1b0
Mohsin <mohsin@example.com>
2026-09-18 00:53:01 +0530

    Fix 404 on root by adding redirect to basePath

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ import { BASE_PATH } from './src/lib/basepath.mjs';
 const nextConfig = {
   basePath: BASE_PATH,
 
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: BASE_PATH,
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
+
   async headers() {
     return [
       {
